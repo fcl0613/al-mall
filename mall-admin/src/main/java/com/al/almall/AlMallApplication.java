7d2989d08cb2b5f6823833a4cc587c04de8b7a55
fcl <fcl@qq.com>
2023-02-14 22:42:54 +0800

    '项目初始化'

---
+++ mall-admin/src/main/java/com/al/almall/AlMallApplication.java
@@ -0,0 +1,11 @@
+package com.al.almall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlMallApplication.class, args);
+    }
+}
